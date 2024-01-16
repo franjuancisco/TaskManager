4429977c45232d34ecc58dfd1d501e472412c94e
Juan Francisco <fran515rocket@gmail.com>
2024-01-15 20:11:13 -0400

    Add project files.

---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskManager.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskController(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+       
+    }
+}
---
+++ TaskManager/Models/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class TaskModel
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Estado { get; set; }
+        public int Prioridad { get; set; }
+    }
+}
